d0bb0ab381b546943a58f30f994431426665379c
sincereliu <sincereliulxs@gmail.com>
2022-07-29 15:41:32 +0800

    增加了https相关的配置

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const path = require('path')
+const https = require('https')
+const { readFileSync } = require('fs')
+
 const app = express()
+
 const port = 3000
 
 // 托管静态文件
@@ -30,7 +34,17 @@ app.delete('/user', function (req, res) {
   res.send('Got a DELETE request at /user')
 })
 
+const options = {
+  key: readFileSync('/root/myapp_cert/myapp.key'),
+  cert: readFileSync('/root/myapp_cert/myapp.cert'),
+  allowHTTP1: true
+}
 
 app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+const httpsserver = https.createSecureServer(options, app)
+httpsserver.listen(3001, () => {
   console.log(`Example app listening on port ${port}`)
 })
